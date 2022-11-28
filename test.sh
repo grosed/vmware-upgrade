12b16e689e8075413e025f60afc918978efd4bc0
Daniel Grose <31761607+grosed@users.noreply.github.com>
2022-11-28 09:50:09 +0000

    Delete test.sh

--- test.sh
+++
@@ -1,4 +0,0 @@
-
-sudo cp /etc/fstab /etc/fstab.saf
-sudo echo //luna.lancs.ac.uk/FST/MA/Stor-i/${USER} ${HOME}/luna cifs domain=LANCS,noauto,rw,users,sec=krb5,vers=3.1.1 0 0 >> /etc/fstab
-mkdir ${HOME}/luna-new
