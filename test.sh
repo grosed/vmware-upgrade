52659099b89bbe472e19bb2d2af0b57857cd1915
Daniel Grose <31761607+grosed@users.noreply.github.com>
2022-11-28 09:26:45 +0000

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,4 @@
+
+cp /etc/fstab /etc/fstab.saf
+echo //luna.lancs.ac.uk/FST/MA/Stor-i/${USER} ${HOME}/luna cifs domain=LANCS,noauto,rw,users,sec=krb5,vers=3.1.1 0 0 >> /etc/fstab
+mkdir ${HOME}/luna-new
